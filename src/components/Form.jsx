29182cf35730f9aa0703e73df63c5218db208656
Evgeny Pankratov <mr.johnny31.90@gmail.com>
2024-12-10 10:51:04 +0400

    HW-final Redux combineReducer

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,38 @@
 import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {api_key, base_url} from "../utils/constants.js";
+import {changeMessage, changeWeather} from "../actions/dataActions.js";
 
 
-const Form = ({getWeather}) => {
+const Form = () => {
     const [city,setCity] = useState('');
     const getCity = (e) => {
         e.preventDefault();
         getWeather(city);
         setCity('');
+    }
 
+
+    const dispatch = useDispatch();
+    const getWeather = (city) => {
+        fetch(`${base_url}?q=${city}&appid=${api_key}&units=metric`)
+            .then(result => result.json())
+            .then(data => {
+                console.log(data)
+                dispatch(changeWeather({
+                        country: data.sys.country,
+                        city: data.name,
+                        temp: data.main.temp,
+                        pressure: data.main.pressure,
+                        sunset: data.sys.sunset
+                    }
+                ))
+                dispatch(changeMessage(''))
+            })
+            .catch(error => {
+                console.log(error)
+                dispatch(changeMessage("Enter correct city name"))
+            });
     }
 
     return (
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -9,7 +9,6 @@ const Weather = () => {
 
 
 
-
     if (message) {
 
         return (
